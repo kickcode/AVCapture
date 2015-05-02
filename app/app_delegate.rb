09ab1a22f86512294bf2358d4a45101425ba215f
Elliott Draper <el@kickcode.com>
2015-05-03 00:59:32 +0100

    session and device inputs/output setup

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,6 +2,24 @@ class AppDelegate
   def applicationDidFinishLaunching(notification)
     buildMenu
     buildWindow
+
+    @session = AVCaptureSession.alloc.init
+    @session.sessionPreset = AVCaptureSessionPresetHigh
+
+    devices = AVCaptureDevice.devices
+    video_device = devices.select { |device| device.hasMediaType(AVMediaTypeVideo) }.first
+    audio_device = devices.select { |device| device.hasMediaType(AVMediaTypeAudio) }.first
+
+    video_input = AVCaptureDeviceInput.deviceInputWithDevice(video_device, error: nil)
+    audio_input = AVCaptureDeviceInput.deviceInputWithDevice(audio_device, error: nil)
+
+    if @session.canAddInput(video_input) && @session.canAddInput(audio_input)
+      @session.addInput(video_input)
+      @session.addInput(audio_input)
+    end
+
+    @output = AVCaptureMovieFileOutput.alloc.init
+    @session.addOutput(@output) if @session.canAddOutput(@output)
   end
 
   def buildWindow
